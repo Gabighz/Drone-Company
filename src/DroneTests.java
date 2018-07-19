398938cc4ecf4cb709a8c68c42ef0554832eab86
Gabighz <ghiuzangabriel2@gmail.com>
2018-07-19 17:14:13 +0100

    Improved variable naming and wrote tests for updateSpeed and updateCoordinates

--- src/DroneTests.java
+++ src/DroneTests.java
@@ -5,23 +5,46 @@
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import java.util.Scanner;
 
 public class DroneTests {
 
     @Test
-    public void droneSpeed() {
-        Drone firstDrone = new Drone();
-        firstDrone.setSpeed(1);
+    public void droneSetSpeed() {
+        Drone someDrone = new Drone();
+        someDrone.setSpeed(1);
 
-        assertEquals(1, firstDrone.getSpeed());
+        assertEquals(1, someDrone.getSpeed());
     }
 
     @Test
-    public void droneCoordinates() {
-        Drone secondDrone = new Drone();
-        secondDrone.setCoordinates(50, 50);
+    public void droneSetCoordinates() {
+        Drone someDrone = new Drone();
+        someDrone.setCoordinates(50, 50);
 
-        assertEquals("50,50", secondDrone.getCoordinates());
+        assertEquals("50,50", someDrone.getCoordinates());
+    }
+
+    @Test
+    public void droneUpdateSpeed() {
+        Drone someDrone = new Drone();
+        someDrone.updateSpeed();
+
+        assertTrue(0 <= someDrone.getSpeed() && someDrone.getSpeed() <= 100);
+    }
+
+    @Test
+    public void droneUpdateCoordinates() {
+        Drone someDrone = new Drone();
+        someDrone.updateCoordinates();
+
+        String currentCoordinates = someDrone.getCoordinates();
+        Scanner line = new Scanner(currentCoordinates).useDelimiter(",");
+        int xCoordinate = line.nextInt();
+        int yCoordinate = line.nextInt();
+
+        assertTrue(0 <= xCoordinate && xCoordinate <= 50);
+        assertTrue(0 <= yCoordinate && yCoordinate <= 50);
     }
 
 }
